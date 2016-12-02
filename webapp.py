b70afaca7df1c114596b007368c9c9e4cb392610
Lisa Heffernan <Lisa Heffernan>
2016-12-02 10:33:55 +0000

    commit

--- webapp.py
+++ webapp.py
@@ -3,22 +3,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
- return app.send_static_file('index.html')
-
+	return app.send_static_file('index.html')
+ 
+@app.route("/home") 
+@app.route('/home/<name>')
+def home(name=None):  
+	return render_template('home.html', name = name)
+ 
+ 
 @app.route("/about") 
-@app.route('/about/<name>') 
 def about(name=None): 
- return render_template('about.html', name = name)
+	return render_template('about.html', name = name)
 	
 @app.route("/gallery") 
-@app.route('/gallery/<name>')
 def gallery(name=None): 
- return render_template('gallery.html', name = name)
+	return render_template('gallery.html', name = name)
 
 @app.route("/contact") 
-@app.route('/contact/<name>')
 def contact(name=None):  
- return render_template('contact.html', name = name)
- 
+	return render_template('contact.html', name = name)
+	
+
+	
+
 if __name__ == "__main__":     
 	app.run()
